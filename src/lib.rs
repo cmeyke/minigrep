737765de919c6cd89a7ad433e5f35e1e436fc7f0
Carsten Meyke <carsten.meyke@gmail.com>
2023-08-22 07:34:22 +0200

    use iterators instead of for loops in search

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,5 @@
 use std::error::Error;
-use std::{env, fs, vec};
+use std::{env, fs};
 
 pub struct Config {
     pub query: String,
@@ -48,28 +48,18 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
-    let mut results = vec![];
-
-    for line in contents.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
     let query = &query.to_lowercase();
-    let mut results = vec![];
-
-    for line in contents.lines() {
-        if line.to_lowercase().contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    contents
+        .lines()
+        .filter(|line| line.to_lowercase().contains(query))
+        .collect()
 }
 
 #[cfg(test)]
